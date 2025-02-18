6ee8e10ef4ac22e4c61aef903376056bfc1d6df6
Akash Gupta <akashgupta_ce_2021@ltce.in>
2025-02-18 23:27:13 +0530

    implemented/API/hightorderFunctions/redux

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { asyncgetproducts } from "../store/actions/ProductsActions";
 
 const Product = () => {
-  return (
-    <div>Product</div>
-  )
-}
+  const { products } = useSelector((state) => state.productReducer);
+  console.log(products);
 
-export default Product
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(asyncgetproducts());
+  }, []);
+
+  return (
+    <>
+      <div className=" bg-red-300 h-full w-1/2 flex flex-col items-start justify-start ml-auto mr-auto p-5 mt-10">
+        <div className="bg-red-500 text-center mt-10 w-60 ml-auto mr-auto  h-20 py-4  ">
+          <h1 className=" text-white text-5xl font-bold">User List </h1>
+        </div>
+
+        <ul className=" mt-5 ml-52 cursor-pointer">
+          {products.map((items, index) => {
+            return (
+              <li key={index} className="">
+                <h1>
+                  {items.id} {items.title}
+                </h1>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Product;
--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './Reducers/userReducer'; // Import the default export (reducer)
+import productReducer from "./Reducers/ProductReducers"
 
 export const store = configureStore({
   reducer: {
-    userReducer : userReducer,   // ye hamara  chote choter reducers hai jinko chota sa state bhi bolte hai example product componrnt cart components etc ye sab chote chote reducres hai or states hai
+    userReducer : userReducer, 
+    productReducer: productReducer,  // ye hamara  chote choter reducers hai jinko chota sa state bhi bolte hai example product componrnt cart components etc ye sab chote chote reducres hai or states hai
 
   },
 });
