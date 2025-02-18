c878d3b1dc6b03f7d59d01794341c3a1fd99b38f
Akash Gupta <akashgupta_ce_2021@ltce.in>
2025-02-18 23:28:28 +0530

    products

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -16,7 +16,7 @@ const Product = () => {
     <>
       <div className=" bg-red-300 h-full w-1/2 flex flex-col items-start justify-start ml-auto mr-auto p-5 mt-10">
         <div className="bg-red-500 text-center mt-10 w-60 ml-auto mr-auto  h-20 py-4  ">
-          <h1 className=" text-white text-5xl font-bold">User List </h1>
+          <h1 className=" text-white text-5xl font-bold">Products List </h1>
         </div>
 
         <ul className=" mt-5 ml-52 cursor-pointer">
