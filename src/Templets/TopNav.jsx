11879ab8462e82476f674f84bb707fddfbd6748f
Akash Gupta <akashgupta_ce_2021@ltce.in>
2025-02-22 04:12:59 +0530

    topnav

--- src/Templets/TopNav.jsx
+++ src/Templets/TopNav.jsx
@@ -5,8 +5,8 @@ const TopNav = () => {
   const [query, setQuery] = useState("");
 
   return (
-    <div className=" w-auto h-[10vh]  relative flex items-center justify-start ml-[15%] mt-3">
-      <i class="ri-search-2-line text-white text-3xl"></i>
+    <div className=" w-auto h-[10vh]  relative flex items-center justify-start  ml-[20%] mt-3">
+      <i class="ri-search-2-line text-white ml-2 -mr-8 text-3xl "></i>
 
       <input
         type="text"
@@ -17,30 +17,21 @@ const TopNav = () => {
       />
 
       {query.length > 0 && (
-        <i  onClick={() => {
+        <i
+          onClick={() => {
             setQuery("");
           }}
           class="ri-close-large-line text-zinc-400 text-3xl"
         ></i>
       )}
-           
-           <div className="absolute w-[50%] max-h-[50vh]  mt-6 ml-3 bg-zinc-200 top-[90%] rounded-md overflow-auto">
 
+      <div className="absolute w-[50%] max-h-[50vh]  mt-6 ml-3 bg-zinc-200 top-[90%] rounded-md overflow-auto">
+        {/* 
      <Link className = " hover:text-black hover-bg-zinc-400 duration-300 w-[100%] font-semibold text-zinc-600  p-8 flex justify-start items-center border-b-2 border-zinc-100" >
             <img src="" alt="" />
             <span>Hello</span>
-            </Link>
-        
-
-           
-           
-
-           </div>
-
-
-
-
-
+            </Link> */}
+      </div>
     </div>
   );
 };
