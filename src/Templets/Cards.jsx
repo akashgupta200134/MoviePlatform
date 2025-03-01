6cfe0337e574d87f052afed30af59e8d73538745
Akash Gupta <akashgupta_ce_20212@ltce.in>
2025-03-02 03:05:45 +0530

    ratings

--- src/Templets/Cards.jsx
+++ src/Templets/Cards.jsx
@@ -3,23 +3,35 @@ import { Link } from "react-router-dom";
 
 const Cards = ({ data, title }) => {
     return (
-        <div className="flex  flex-wrap gap-5 bg-[#1f1e24] items-center justify-center w-[100%]    h-full ">
+        <div className="flex  relative  flex-wrap gap-5 bg-[#1f1e24] items-center justify-center w-[100%]    h-full ">
             {data.map((item, index) => (
                 <Link
                     to={`/details/${item.id}`}
                     className="w-[50vh]"
                     key={index}
                 >
+                   {item.vote_average && (
+                    <span className="absolute rounded-full text-lg  mt-32 ml-[245px]    p-2 font-semibold bg-yellow-600  ">
+                            {(item.vote_average * 10).toFixed()}<sup>%</sup> 
+                        </span>
+                   )}
+                    
                     <img
                         className="h-[30vh] rounded-lg object-cover max-w-full"
                         src={`https://image.tmdb.org/t/p/original/${
                             item.backdrop_path || item.poster_path || item.profile_path
                         }`}
                         alt=""
+                        
                     />
                     <h1 className="text-xl text-center text-white mt-3 font-semibold">
                         {item.title || item.original_title || item.name || item.original_name}
+
+                       
+
                     </h1>
+                         
+                   
                 </Link>
             ))}
         </div>
